206741c2b222556524967fdef1134c9b51a2c297
Muruga Boopathy <Muruga Boopathy@LAPTOP-E03PEACM>
2022-11-07 16:29:55 +0530

    uploding my code.

--- src/p1/Demo.java
+++ src/p1/Demo.java
@@ -5,8 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(6);
+		System.out.println(10);
 		System.out.println(3);
+		System.out.println(15);
 	}
 
 }
